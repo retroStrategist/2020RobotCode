1484b5fd25dabab969f3b1b4e7ea2819d76b9e8b
Christopher Moore <chmoore889@gmail.com>
2020-01-30 22:46:07 -0500

    Added basic color sensor stuff

--- src/main/java/frc/robot/subsystems/ControlPanel.java
+++ src/main/java/frc/robot/subsystems/ControlPanel.java
@@ -1,11 +1,22 @@
 package frc.robot.subsystems;
 
-public class ControlPanel { 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.I2C.Port;
+
+import com.revrobotics.ColorSensorV3;
+
+public class ControlPanel {
+    
+    private WPI_TalonSRX motor1;
+    private WPI_TalonSRX motor2;
+    
+    private ColorSensorV3 colorSensor;
 
     public ControlPanel(){
-
+        motor1 = new WPI_TalonSRX(10);
+        motor2 = new WPI_TalonSRX(11);
         
+        colorSensor = new ColorSensorV3(Port.kOnboard);
     }
-
-
 }
