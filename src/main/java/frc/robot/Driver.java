04386e229d55a80bf8829c4118626a2355139cc0
Christopher Moore <chmoore889@gmail.com>
2020-02-01 15:49:48 -0500

    Added braking and coasting

--- src/main/java/frc/robot/Driver.java
+++ src/main/java/frc/robot/Driver.java
@@ -11,27 +11,63 @@ public class Driver {
     private SendableChooser<Byte> driveType;
     private final Byte arcade = 0;
     private final Byte tank = 1;
+    
+    SendableChooser<Byte> neutralModeType;//Sets whether motors brake or coast
+    private final Byte brake = 0;
+    private final Byte coast = 1;
 
     public Driver(int port) {
         joy = new Controller(port);
         drive = new Drivetrain();
 
+        //Init driveType
         driveType = new SendableChooser<>();
         driveType.setDefaultOption("Arcade", arcade);
         driveType.addOption("Tank", tank);
         SmartDashboard.putData("Drive Type", driveType);
+        
+        //Init neutralModelType
+        neutralModeType = new SendableChooser<>();
+        neutralModeType.setDefaultOption("Brake", brake);
+        neutralModeType.addOption("Coast", coast);
+        SmartDashboard.putData("Drivetrain Coast Type", neutralModeType);
     }
 
     public void runDriveControls() {
+        driveType();
+        neutralModeType();
+        faultCheck();
+    }
+    
+    //Selects driveType and drives
+    private void driveType() {
         if(driveType.getSelected().equals(arcade)) {
             drive.arcadeDrive(joy.getRightYAxis(), joy.getLeftTrigger(), joy.getRightTrigger());
             System.out.println("Y Axis: " + joy.getRightYAxis() + "     Left: " + joy.getLeftTrigger() + "     Right: " + joy.getRightYAxis());
-        } else if(driveType.getSelected().equals(tank)) {
+        } 
+        else if(driveType.getSelected().equals(tank)) {
             drive.tankDrive(joy.getLeftYAxis(), joy.getRightYAxis());
-        } else {
+        } 
+        else {
             System.out.println("Error: No drive type chosen");
         }
-        
+    }
+    
+    //Selects coast/brake
+    private void neutralModeType() {
+        if(neutralModeType.getSelected().equals(brake)) {
+            drive.setNeutralMode(NeutralMode.Brake);
+        }
+        else if(neutralModeType.getSelected().equals(coast)) {
+            drive.setNeutralMode(NeutralMode.Coast);
+        }
+        else {
+            System.out.println("Error: No coast type chosen");
+        }
+    }
+    
+    //Checks for driveTrain motor faults
+    private void faultCheck() {
         if(drive.isFault()) {
             System.out.print(drive.getFaults());
         }
