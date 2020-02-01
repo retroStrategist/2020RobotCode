f9aa252801f3f04ef632b408b2af2036fb5c871d
Christopher Moore <chmoore889@gmail.com>
2020-02-01 17:36:51 -0500

    Added slew rate filter

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -6,6 +6,7 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.SlewRateLimiter;
 
 public class Drivetrain {
 
