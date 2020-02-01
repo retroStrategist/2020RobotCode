26204ebbdaa3970f49a095c560cbb3627a8d572c
Team6423 <55815254+Team6423@users.noreply.github.com>
2020-02-01 13:43:19 -0500

    Update ControlPanel.java

--- src/main/java/frc/robot/subsystems/ControlPanel.java
+++ src/main/java/frc/robot/subsystems/ControlPanel.java
@@ -10,33 +10,61 @@ import edu.wpi.first.wpilibj.I2C.Port;
 import com.revrobotics.ColorSensorV3;
 import com.revrobotics.ColorSensorV3.RawColor;
 
+import java.lang.Math;
+
 public class ControlPanel {
     
-    private WPI_TalonSRX motor1;
-    private WPI_TalonSRX motor2;
+    private WPI_TalonSRX arm;
+    private WPI_TalonSRX spin;
     
     private ColorSensorV3 colorSensor;
+        
+    private final double SPIN_MOTOR_SPEED = 0.5;
     
-    private final RawColor BLUE = new RawColor(0,0,0,0);//FIND VALUES FOR HERE
-    private final RawColor GREEN = new RawColor(0,0,0,0);//FIND VALUES FOR HERE
-    private final RawColor RED = new RawColor(0,0,0,0);//FIND VALUES FOR HERE
-    private final RawColor YELLOW = new RawColor(0,0,0,0);//FIND VALUES FOR HERE
+    private final PossibleColor colorOrder[];
     
-    enum PossibleColor
+    
+    private enum PossibleColor
     { 
-        BLUE, GREEN, RED, YELLOW; 
+        //TODO find correct color values
+        BLUE(new RawColor(0, 0, 0, 0)),
+        GREEN(new RawColor(0, 0, 0, 0)),
+        RED(new RawColor(0, 0, 0, 0)),
+        YELLOW(new RawColor(0, 0, 0, 0)); 
+        
+        private final RawColor color;
+        
+        PossibleColor(RawColor color) {
+            this.color = color;
+        }
+        
+        public RawColor getColor() {
+            return color;
+        }
     } 
 
     public ControlPanel() {
-        motor1 = new WPI_TalonSRX(10);
-        motor2 = new WPI_TalonSRX(11);
+        arm = new WPI_TalonSRX(10);
+        spin = new WPI_TalonSRX(11);
         
         colorSensor = new ColorSensorV3(Port.kOnboard);
+        
+        colorOrder = new PossibleColor[4];
+        colorOrder[0] = PossibleColor.BLUE;
+        colorOrder[1] = PossibleColor.GREEN;
+        colorOrder[2] = PossibleColor.RED;
+        colorOrder[3] = PossibleColor.YELLOW;
     }
     
-    //Turn wheel until the correct color is reached
-    public void positionControl() {
-        
+    //Turns wheel when the correct color is not reached & returns true if position is reached
+    public boolean positionControl() {
+        if(fieldSensorColor() != getIntendedColor()) {
+            spin.set(SPIN_MOTOR_SPEED);
+            return false;
+        } else {
+            spin.set(0.0);
+            return true;
+        }
     }
     
     //Turn wheel specified number of times
@@ -44,12 +72,28 @@ public class ControlPanel {
         
     }
     
-    //Return RawColor currently seen by color sensor
+    //Return RawColor currently seen by field sensor based on color seen by the robot sensor
+    private PossibleColor fieldSensorColor() {
+        return colorOrder[(x + 2)%4];
+    }
+
+    //returns corresponding array index
+    private int getArrayIndex() {
+        PossibleColor color = findCloseColor();
+        for(int x = 0; x < 4; x++) {
+            if(color == colorOrder[x]) {
+                return x;
+            }
+        }
+    }
+    
+    //Returns RawColor currently seen by color sensor
+    //Only a utility function to help other methods
+    //Use findCloseColor() instead
     private RawColor getCurrentColor() {
         return colorSensor.getRawColor();
     }
     
-    //Return target PossibleColor sent by FMS
     private PossibleColor getIntendedColor() {
         String gameData;
         gameData = DriverStation.getInstance().getGameSpecificMessage();
@@ -73,14 +117,45 @@ public class ControlPanel {
         }
     }
     
-    //Returns closest PossibleColor
+    //Returns closest PossibleColor to current sensor color
     private PossibleColor findCloseColor() {
+        RawColor currColor = getCurrentColor();
+        blueDiff = colorDifference(currColor, PossibleColor.BLUE.getColor());
+        greenDiff = colorDifference(currColor, PossibleColor.GREED.getColor());
+        redDiff = colorDifference(currColor, PossibleColor.RED.getColor());
+        yellowDiff = colorDifference(currColor, PossibleColor.YELLOW.getColor());
         
+        int temp, size;
+        int array[] = {blueDiff, greenDiff, redDiff, yellowDiff};
+        size = array.length;
+        
+        for(int i = 0; i < size; i++) {
+            for(int j = i+1; j < size; j++) {
+                if(array[i] > array[j]) {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        
+        switch (array[0]) {
+            case blueDiff:
+                return PossibleColor.BLUE;
+            case greenDiff:
+                return PossibleColor.GREEN;
+            case redDiff:
+                return PossibleColor.RED;
+            case yellowDiff:
+                return PossibleColor.YELLOW;
+        }
+        return NULL;
     }
     
-    //Returns difference in raw color value
-    private int colorDifference() {
-        
+    //Returns the sum of each in raw color value of inputed color and PossibleColors
+    private int colorDifference(RawColor color, RawColor possible) {
+        return Math.abs(color.red - possible.red) + Math.abs(color.green - possible.green) +
+            Math.abs(color.blue - possible.blue) + Math.abs(color.ir - possible.ir);
     }
 
 }
