7629d29e13c913f7b039b10245c105332c3ea221
Christopher Moore <chmoore889@gmail.com>
2020-02-01 19:03:56 -0500

    Added rate filters

--- src/main/java/frc/robot/subsystems/Wheels.java
+++ src/main/java/frc/robot/subsystems/Wheels.java
@@ -4,43 +4,71 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.SlewRateLimiter;
+
 public class Wheels { 
 
     private WPI_TalonSRX intake;
     private WPI_TalonSRX largeWheel;
     private WPI_TalonSRX shooter;
+    
+    //Maximum rate at which motor speed can change
+    private final double INTAKE_RATE_LIMIT = 2;
+    private final double LARGE_WHEEL_RATE_LIMIT = 1;
+    private final double SHOOTER_RATE_LIMIT = 2;
+    
+    private final SlewRateLimiter intakeRateFilter;
+    private final SlewRateLimiter largeWheelRateFilter;
+    private final SlewRateLimiter shooterRateFilter;
 
     public Wheels(){
         intake = new WPI_TalonSRX(4);
         largeWheel = new WPI_TalonSRX(5);
         shooter = new WPI_TalonSRX(6);
+        
+        intakeRateFilter = new SlewRateLimiter(INTAKE_RATE_LIMIT);
+        largeWheelRateFilter = new SlewRateLimiter(LARGE_WHEEL_RATE_LIMIT);
+        shooterRateFilter = new SlewRateLimiter(SHOOTER_RATE_LIMIT);
     }
     //forward is from intake to shooters
     public void spinBigWheelFor(){
-        largeWheel.set(ControlMode.PercentOutput, -0.3);
+        setBigWheel(-0.3);
     }
 
     public void spinBigWheelBac(){
-        largeWheel.set(ControlMode.PercentOutput, 0.3);
+        setBigWheel(0.3);
     }
     
     public void stopBigWheel(){
-        largeWheel.set(ControlMode.PercentOutput, 0.0);
+        setBigWheel(0);
+    }
+    
+    //Sets speed of big wheel
+    private void setBigWheel(double speed) {
+        largeWheel.set(ControlMode.PercentOutput, largeWheelRateFilter.calculate(speed));
     }
     
     public void spinIntake(){
-        intake.set(ControlMode.PercentOutput, -0.5);
+        setIntake(-0.5);
     }
     
     public void stopIntake(){
-        intake.set(ControlMode.PercentOutput, 0.0);
+        setIntake(0);
+    }
+    
+    private void setIntake(double speed) {
+        intake.set(ControlMode.PercentOutput, intakeRateFilter.calculate(speed));
     }
     
     public void spinShooter(){
-        shooter.set(ControlMode.PercentOutput, 0.85);
+        setShooter(0.85);
     } 
     
     public void stopShooter(){
-        shooter.set(ControlMode.PercentOutput, 0.0);
+        setShooter(0);
+    }
+    
+    private void setShooter(double speed) {
+        shooter.set(ControlMode.PercentOutput, shooterRateFilter.calculate(speed));
     }
 }
